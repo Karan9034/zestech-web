592ba7d3f247c322c72648d46b2dc46d10256538
Karan9034 <karan.agr9034@gmail.com>
2020-12-18 21:27:59 +0530

    Heroku Trial1

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config():
-    SECRET_KEY = ''
+    SECRET_KEY = 'c9baac77-7603-488e-a2a0-36759b44f0c0'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_USERNAME = 'inquiry.zestech@gmail.com'
+    MAIL_PASSWORD = '1234Abcd9@'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'files')
