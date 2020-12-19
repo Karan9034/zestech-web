bd9334772fd3f9b4101d2cf031d4f1fe17fd312a
Karan9034 <karan.agr9034@gmail.com>
2020-12-19 17:43:00 +0530

    Fixes

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config():
-    SECRET_KEY = 'c9baac77-7603-488e-a2a0-36759b44f0c0'
+    SECRET_KEY = ''
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'inquiry.zestech@gmail.com'
-    MAIL_PASSWORD = '1234Abcd9@'
+    MAIL_PASSWORD = ''
