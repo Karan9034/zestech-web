abeb4010cca0597ee70513bf100ed61201925480
Karan9034 <karan.agr9034@gmail.com>
2020-12-19 15:55:07 +0530

    Heroku Trial 10

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config():
     SECRET_KEY = 'c9baac77-7603-488e-a2a0-36759b44f0c0'
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'inquiry.zestech@gmail.com'
