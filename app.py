216916703acb24e2ed18fcbe8808aa9db8aafa3d
Karan9034 <karan.agr9034@gmail.com>
2020-12-20 12:39:23 +0530

    Mail service changed

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-import os
+import os, magic
 from flask import Flask, render_template, url_for, redirect, request, flash
 from form import QueryForm
-from mail import send_mail
-from flask_mail import Mail
+from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mail = Mail(app)
+
+def send_mail(form):
+	msg = Message("Quote Enquiry Mail", sender='inquiry.zestech@gmail.com', recipients=['karan.agr9034@gmail.com'])
+	msg.body = f'''Email: {form.email.data}
+Phone: {form.phone.data}
+Address: {form.address.data}
+Average Monthly Bill: {form.bill.data}
+Message: {form.message.data}
+'''
+	for file in os.listdir(os.path.join(os.getcwd(), 'files')):
+		with open(os.path.join(os.getcwd(), 'files', file)) as fp:
+			mime = magic.Magic(mime=True)
+			c_type = mime.from_file(os.path.join(os.getcwd(), 'files', file))
+			msg.attach(file, c_type, fp.read())
+	mail.send(msg)
 
 @app.route('/', methods=['GET','POST'])
 def home():
@@ -18,8 +32,7 @@ def home():
 		os.system('mkdir ./files')
 		for file in files:
 			file.save(os.path.join(os.getcwd(), 'files', secure_filename(file.filename)))
-		msg = send_mail(form)
-		mail.send(msg)
+		send_mail(form)
 		os.system('rm -r ./files/*')
 		flash('We will reach out to you in a few days', 'success')
 		return redirect(url_for('home'))
--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config():
     SECRET_KEY = ''
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'inquiry.zestech@gmail.com'
+    MAIL_USERNAME = ''
     MAIL_PASSWORD = ''
