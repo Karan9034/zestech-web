b3dc6c582240b01660a6b7a6f6b166fe3864fdde
Karan9034 <karan.agr9034@gmail.com>
2020-12-20 15:05:08 +0530

    Final Trial

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config():
-    SECRET_KEY = ''
+    SECRET_KEY = 'c9baac77-7603-488e-a2a0-36759b44f0c0'
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_USERNAME = 'zestech.inquiry@outlook.com'
+    MAIL_PASSWORD = '1234Abcd9@'
